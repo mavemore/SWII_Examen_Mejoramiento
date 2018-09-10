9cfb3a32ad8915fc6415935a615670f97d5373e1
Johan Alejandro <johanalejandro@me.com>
2018-09-10 09:55:46 -0500

    Mejoramiento

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
+
+def aprobar_prestamo(a_trabajo,capacidad_endeudamiento,estado_civil,numero_dependientes,a_vivienda):
+	estado_solicitud=""
+	if(a_trabajo >= 2):
+		if(estado_civil == "Divorced"):
+			if(numero_dependientes>0):
+				estado_solicitud="Aprobada"
+			elif(numero_dependientes==0):
+				estado_solicitud = "Revision Manual"
+		elif(estado_civil == "Single"):
+			estado_solicitud = "Denegada"
+		elif(estado_civil == "Married"):
+			estado_solicitud = "Aprobada"
+	elif(a_trabajo <2):
+		if(capacidad_endeudamiento>=75):
+			estado_solicitud = "Revision Manual"
+		elif(capacidad_endeudamiento <75):
+			if(a_vivienda < 1.5):
+				estado_solicitud = "Denegada"
+			elif(a_vivienda >= 1.5):
+				estado_solicitud = "Aprobada"
+
+	return estado_solicitud
+
+
+
+
--- test.py
+++ test.py
@@ -12,6 +12,31 @@ class Test(unittest.TestCase):
     
     def test_prueba_ID(self):
         self.assertEquals()
+    def test_prueba_2(self):
+    	resultado = aprobar_prestamo(3,75,"Divorced",3,4)
+    	print(resultado)
+        self.assertEquals(resultado,"Aprobada")
+    def test_prueba_3(self):
+    	resultado = aprobar_prestamo(3,75,"Divorced",0,4)
+        self.assertEquals(resultado,"Revision Manual")
+    def test_prueba_4(self):
+    	resultado = aprobar_prestamo(3,75,"Single",3,4)
+        self.assertEquals(resultado,"Denegada")
+    def test_prueba_5(self):
+    	resultado = aprobar_prestamo(3,75,"Married",3,4)
+        self.assertEquals(resultado,"Aprobada")
+    def test_prueba_6(self):
+    	resultado = aprobar_prestamo(1,75,"Married",3,4)
+        self.assertEquals(resultado,"Revision Manual")
+    def test_prueba_7(self):
+    	resultado = aprobar_prestamo(1,70,"Married",3,1)
+        self.assertEquals(resultado,"Denegada")
+    def test_prueba_8(self):
+    	resultado = aprobar_prestamo(1,70,"Married",3,4)
+        self.assertEquals(resultado,"Aprobada")
+
+
+
             
 if __name__ == '__main__':
     unittest.main()
