d2aed00a611ced880a529dea5abc71737ef59481
jorge18link <jorge_18link@hotmail.com>
2018-09-10 09:37:25 -0500

    funcionalidades y pruebas

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobar_prestamo(aniosTrabajo, estadoCivil, capacidadEndeuda, aniosVivienda, dependientes):
+	
+	estado = 'D'
+	if aniosTrabajo < 2:
+		if capacidadEndeuda < 75:
+			if aniosVivienda < 1.5:
+				estado = 'D'
+			else:
+				estado = 'A'
+		else:
+			estado = 'M'
+	else:
+		if estadoCivil == 'divorciado':
+			if dependientes > 0:
+				estado = 'A'
+			if dependientes == 0:
+				estado = 'M'
+		elif estadoCivil == 'soltero':
+			estado = 'D'
+		elif estadoCivil == 'casado':
+			estado = 'A'
+		    
+	return estado
+
--- test.py
+++ test.py
@@ -2,16 +2,44 @@
 from __future__ import unicode_literals
 
 import unittest
-import funcionalidad
+import funcionalidad as fun
 
 
 class Test(unittest.TestCase):
-    
+	
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_prueba_ID(self):
-        self.assertEquals()
-            
+	#aprobar_prestamo(aniosTrabajo, estadoCivil, capacidadEndeuda, aniosVivienda, dependientes)
+	def test_prueba_C01(self):
+		result = fun.aprobar_prestamo(1,'casado',65,1,1)
+		self.assertEquals(result,'D')
+		
+	def test_prueba_C02(self):
+		result = fun.aprobar_prestamo(1,'casado',65,2,1)
+		self.assertEquals(result,'A')
+		
+	def test_prueba_C03(self):
+		result = fun.aprobar_prestamo(1,'casado',75,1,1)
+		self.assertEquals(result,'M')
+		
+	def test_prueba_C04(self):
+		result = fun.aprobar_prestamo(2,'divorciado',75,1,1)
+		self.assertEquals(result,'A')
+		
+	def test_prueba_C05(self):
+		result = fun.aprobar_prestamo(2,'divorciado',75,1,0)
+		self.assertEquals(result,'M')
+		
+	def test_prueba_C06(self):
+		result = fun.aprobar_prestamo(2,'soltero',75,1,0)
+		self.assertEquals(result,'D')
+		
+	def test_prueba_C07(self):
+		result = fun.aprobar_prestamo(2,'casado',75,1,0)
+		self.assertEquals(result,'A')
+		
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
+	
+	
+	
