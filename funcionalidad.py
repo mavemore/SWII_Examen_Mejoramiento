b2186606244cbf104c29108956e3125a585a996f
RoberthXLZ <roberthloor1@gmail.com>
2018-09-10 09:10:36 -0500

    Ejercicio 6

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobarPrestamo(anioDeTrabajo, estadoCivil, capEndeudamiento, aniosVivienda, dependientes):
+	aprobado = "No"
+	if anioDeTrabajo >= 2:
+		if estadoCivil == "married":
+			aprobado= "A"
+		elif estadoCivil == "single":
+			aprobado = "D"
+		elif estadoCivil == "divorced":
+			if dependientes == 0:
+				aprobado= "Se necesita mas información"
+			elif dependientes > 0:
+				aprobado = "A"
+	elif anioDeTrabajo < 2:
+		if capEndeudamiento < 75:
+			if aniosVivienda <1.5:
+				aprobado="D"
+			elif aniosVivienda >=1.5:
+				aprobado ="A"
+		elif capEndeudamiento >= 75:
+			aprobado="A"
+	return aprobado
+
--- test.py
+++ test.py
@@ -10,8 +10,20 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_prueba_ID(self):
-        self.assertEquals()
+    def test_prueba_ID1(self):
+        self.assertEquals((funcionalidad.aprobarPrestamo(2, "married", 75, 1, 0)),"A")
+    def test_prueba_ID2(self):
+        self.assertEquals((funcionalidad.aprobarPrestamo(2, "single", 75, 1, 0)),"D")
+    def test_prueba_ID3(self):
+        self.assertEquals((funcionalidad.aprobarPrestamo(2, "divorced", 75, 1, 0)),"Se necesita mas información")
+    def test_prueba_ID4(self):
+        self.assertEquals((funcionalidad.aprobarPrestamo(2, "divorced", 75, 1, 2)),"A")
+    def test_prueba_ID5(self):
+        self.assertEquals((funcionalidad.aprobarPrestamo(1, "divorced", 60, 1, 2)),"D")
+    def test_prueba_ID6(self):
+        self.assertEquals((funcionalidad.aprobarPrestamo(1, "divorced", 60, 3, 2)),"A")
+    def test_prueba_ID7(self):
+        self.assertEquals((funcionalidad.aprobarPrestamo(1, "divorced", 78, 3, 2)),"A")
             
 if __name__ == '__main__':
     unittest.main()
