b79c577376e20d4e5c84bea018fa83c86bc55b64
jgorotiz <jgorotiz@espol.edu.ec>
2018-09-10 09:14:51 -0500

    Mejoramiento

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def AprobarPrestamo(anios_trabajo, capacidad_endeudamiento, estado_civil, dependientes, anios_vivencia_actual):
+    if anios_trabajo < 2:
+        if capacidad_endeudamiento < 75:
+            if anios_vivencia_actual < 1.5:
+                return "Denegado"
+            else:
+                return "Aprobado"
+        else:
+            return "Revision Manual"
+    elif anios_trabajo >= 2:
+        if estado_civil.lower() == "casado":
+            return "Aprobado"
+        elif estado_civil.lower() == "soltero":
+            return "Denegado"
+        elif estado_civil.lower() == "divorciado":
+            if dependientes == 0:
+                return "Revision Manual"
+            elif dependientes > 0:
+                return "Aprobado"
+            else:
+                return "Numero de dependientes incorrectos."
+        else:
+            return "Estado Civil incorrecto."
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from funcionalidad import *
 import unittest
 import funcionalidad
 
@@ -10,8 +10,41 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_prueba_ID(self):
-        self.assertEquals()
-            
+    def test_prueba_1(self):
+        estado = AprobarPrestamo(1,75,"casado",0,1.5)
+        self.assertEquals(estado, "Revision Manual")
+
+    def test_prueba_2(self):
+        estado = AprobarPrestamo(1,70,"casado",0,1.5)
+        self.assertEquals(estado, "Aprobado")
+
+    def test_prueba_3(self):
+        estado = AprobarPrestamo(1,70,"casado",0,1.4)
+        self.assertEquals(estado, "Denegado")
+
+    def test_prueba_4(self):
+        estado = AprobarPrestamo(2,70,"casado",0,1.4)
+        self.assertEquals(estado, "Aprobado")
+
+    def test_prueba_5(self):
+        estado = AprobarPrestamo(2,70,"Soltero",0,1.4)
+        self.assertEquals(estado, "Denegado")
+
+    def test_prueba_6(self):
+        estado = AprobarPrestamo(2,70,"viudo",0,1.4)
+        self.assertEquals(estado, "Estado Civil incorrecto.")
+
+    def test_prueba_7(self):
+        estado = AprobarPrestamo(2,70,"divorciado",0,1.4)
+        self.assertEquals(estado, "Revision Manual")
+
+    def test_prueba_8(self):
+        estado = AprobarPrestamo(2,70,"divorciado",2,1.4)
+        self.assertEquals(estado, "Aprobado")
+
+    def test_prueba_9(self):
+        estado = AprobarPrestamo(2,70,"divorciado",-2,1.4)
+        self.assertEquals(estado, "Numero de dependientes incorrectos.")
+
 if __name__ == '__main__':
     unittest.main()
