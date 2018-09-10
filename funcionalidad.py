bc32945f6761dba83f68887f963bdda3b1bf2093
Xavier Idrovo <xidrovo@hotmail.com>
2018-09-10 10:27:37 -0400

    respuesta
    :dancers:

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobarPrestamo (numero_dependientes, estado_civil, capacidad_endeudamiento, a_vivienda, a_trabajo):
+	if (a_trabajo < 2):
+		if (capacidad_endeudamiento < 75):
+			if (a_vivienda < 1.5):
+				return 'Denegado'
+			else:
+				return 'Aprobado'
+		else:
+			return 'Manual'
+	else:
+		if (estado_civil == 'D'):
+			if (numero_dependientes > 0):
+				return 'Aprobado'
+			if (numero_dependientes == 0):
+				return 'Manual'
+		elif (estado_civil == 'S'):
+				return 'Denegado'
+		elif (estado_civil == 'C'):
+				return 'Aprobado'
--- test.py
+++ test.py
@@ -2,16 +2,80 @@
 from __future__ import unicode_literals
 
 import unittest
-import funcionalidad
+import funcionalidad as funcionalidad
 
 
 class Test(unittest.TestCase):
-    
-# >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
-# Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_prueba_ID(self):
-        self.assertEquals()
-            
+
+	def test_CC_1(self):
+		numero_dependientes = 0
+		estado_civil = 'S'
+		capacidad_endeudamiento = 70
+		a_vivienda = 1
+		a_trabajo = 1
+
+		resultado = funcionalidad.aprobarPrestamo (numero_dependientes, estado_civil, capacidad_endeudamiento, a_vivienda, a_trabajo)
+		self.assertEqual(resultado, 'Denegado')
+
+	def test_CC_2(self):
+		numero_dependientes = 0
+		estado_civil = 'S'
+		capacidad_endeudamiento = 70
+		a_vivienda = 3
+		a_trabajo = 1
+		
+		resultado = funcionalidad.aprobarPrestamo (numero_dependientes, estado_civil, capacidad_endeudamiento, a_vivienda, a_trabajo)
+		self.assertEqual(resultado, 'Aprobado')
+
+	def test_CC_3(self):
+		numero_dependientes = 0
+		estado_civil = 'S'
+		capacidad_endeudamiento = 80
+		a_vivienda = 1
+		a_trabajo = 1
+		
+		resultado = funcionalidad.aprobarPrestamo (numero_dependientes, estado_civil, capacidad_endeudamiento, a_vivienda, a_trabajo)
+		self.assertEqual(resultado, 'Manual')
+
+	def test_CC_4(self):
+		a_trabajo = 3
+		capacidad_endeudamiento = 70
+		a_vivienda = 1
+		estado_civil = 'D'
+		numero_dependientes = 2
+
+		resultado = funcionalidad.aprobarPrestamo (numero_dependientes, estado_civil, capacidad_endeudamiento, a_vivienda, a_trabajo)
+		self.assertEqual(resultado, 'Aprobado')
+
+	def test_CC_5(self):
+		a_trabajo = 3
+		capacidad_endeudamiento = 70
+		a_vivienda = 1
+		estado_civil = 'D'
+		numero_dependientes = 0
+
+		resultado = funcionalidad.aprobarPrestamo (numero_dependientes, estado_civil, capacidad_endeudamiento, a_vivienda, a_trabajo)
+		self.assertEqual(resultado, 'Manual')
+
+	def test_CC_6(self):
+		a_trabajo = 3
+		capacidad_endeudamiento = 70
+		a_vivienda = 1
+		estado_civil = 'S'
+		numero_dependientes = 2
+
+		resultado = funcionalidad.aprobarPrestamo (numero_dependientes, estado_civil, capacidad_endeudamiento, a_vivienda, a_trabajo)
+		self.assertEqual(resultado, 'Denegado')
+
+	def test_CC_7(self):
+		a_trabajo = 3
+		capacidad_endeudamiento = 70
+		a_vivienda = 1
+		estado_civil = 'C'
+		numero_dependientes = 2
+
+		resultado = funcionalidad.aprobarPrestamo (numero_dependientes, estado_civil, capacidad_endeudamiento, a_vivienda, a_trabajo)
+		self.assertEqual(resultado, 'Aprobado')
+						
 if __name__ == '__main__':
-    unittest.main()
+		unittest.main()
