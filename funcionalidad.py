711ad59cb3d19aec13f3f43401b0ceb15197e93f
soccerdroid <mguarand@espol.edu.ec>
2018-09-10 09:18:14 -0500

    Pruebas de caja blanca hechas

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobar_prestamo(anios_trabajo,cap_endeudamiento,estado_civil,anios_vivienda,dependientes):
+	estado=""
+	if(anios_trabajo<2): #1
+		if(cap_endeudamiento<75): #2
+			if(anios_vivienda<1.5): #3
+				estado="D" #4
+			else:
+				estado="A"#5
+		else:
+			estado="M" #6
+	else:
+		if(estado_civil=="Married"): #7
+			estado="A"#8
+		elif(estado_civil=="Single"):#9
+			estado="D"#10
+		else:
+			if(dependientes>0):#11
+				estado="A"#12
+			else:
+				estado="M"#13
+	return(estado)#f
--- test.py
+++ test.py
@@ -2,16 +2,77 @@
 from __future__ import unicode_literals
 
 import unittest
-import funcionalidad
+from funcionalidad import aprobar_prestamo
 
 
 class Test(unittest.TestCase):
     
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_prueba_ID(self):
-        self.assertEquals()
+#anios_trabajo,cap_endeudamiento,estado_civil,anios_vivienda,dependientes
+    def test_prueba_1(self):
+        anios_trabajo = 1
+        cap_endeudamiento = 74
+        estado_civil = "Divorced"
+        anios_vivienda = 1.4
+        dependientes = 0
+        estado_prestamo = aprobar_prestamo(anios_trabajo,cap_endeudamiento, estado_civil, anios_vivienda, dependientes)
+        self.assertEquals(estado_prestamo,"D")
+
+    def test_prueba_2(self):
+        anios_trabajo = 1
+        cap_endeudamiento = 74
+        estado_civil = "Married"
+        anios_vivienda = 1.5
+        dependientes = 2
+        estado_prestamo = aprobar_prestamo(anios_trabajo,cap_endeudamiento, estado_civil, anios_vivienda, dependientes)
+        self.assertEquals(estado_prestamo,"A")
+
+    def test_prueba_3(self):
+        anios_trabajo = 1
+        cap_endeudamiento = 75
+        estado_civil = "Divorced"
+        anios_vivienda = 1.4
+        dependientes = 1
+        estado_prestamo = aprobar_prestamo(anios_trabajo,cap_endeudamiento, estado_civil, anios_vivienda, dependientes)
+        self.assertEquals(estado_prestamo,"M")
+
+    def test_prueba_4(self):
+        anios_trabajo = 2
+        cap_endeudamiento = 74
+        estado_civil = "Married"
+        anios_vivienda = 1.4
+        dependientes = 0
+        estado_prestamo = aprobar_prestamo(anios_trabajo,cap_endeudamiento, estado_civil, anios_vivienda, dependientes)
+        self.assertEquals(estado_prestamo,"A")
+
+    def test_prueba_5(self):
+        anios_trabajo = 2
+        cap_endeudamiento = 74
+        estado_civil = "Single"
+        anios_vivienda = 1.4
+        dependientes = 0
+        estado_prestamo = aprobar_prestamo(anios_trabajo,cap_endeudamiento, estado_civil, anios_vivienda, dependientes)
+        self.assertEquals(estado_prestamo,"D")
+
+    def test_prueba_6(self):
+        anios_trabajo = 2
+        cap_endeudamiento = 74
+        estado_civil = "Divorced"
+        anios_vivienda = 1.4
+        dependientes = 1
+        estado_prestamo = aprobar_prestamo(anios_trabajo,cap_endeudamiento, estado_civil, anios_vivienda, dependientes)
+        self.assertEquals(estado_prestamo,"A")
+
+    def test_prueba_7(self):
+        anios_trabajo = 2
+        cap_endeudamiento = 74
+        estado_civil = "Divorced"
+        anios_vivienda = 1.4
+        dependientes = 0
+        estado_prestamo = aprobar_prestamo(anios_trabajo,cap_endeudamiento, estado_civil, anios_vivienda, dependientes)
+        self.assertEquals(estado_prestamo,"M")
             
 if __name__ == '__main__':
     unittest.main()
+
