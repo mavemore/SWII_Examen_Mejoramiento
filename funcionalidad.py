940b1820ec4817bff384e403b68733c3f266e49c
Galo Castillo <gadacast@espol.edu.ec>
2018-09-10 09:58:48 -0500

    Modifico funcionalidad

--- funcionalidad.py
+++ funcionalidad.py
@@ -1,3 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+def aprobarPrestamo(dependientes, estadoCivil, capacidadEndeudamiento, aniosVivienda, aniosTrabajo, edad, email):
+	if(edad < 18):
+		return 'SOLICITUD DENEGADA'
+	if(aniosTrabajo < 2):
+		if(capacidadEndeudamiento >= 0.75):
+			return 'SOLICITUD MANUAL'
+		else:
+			if(aniosVivienda < 1.5):
+				return 'SOLICITUD DENEGADA.'
+			else:
+				return 'SOLICITUD APROBADA'
+	else:
+		if(estadoCivil == 'soltero'):
+			return 'SOLICITUD DENEGADA'
+		elif(estadoCivil == 'casado'):
+			return 'SOLICITUD APROBADA'
+		elif(estadoCivil == 'divorciado'):
+			if(dependientes == 0):
+				return 'SOLICITUD MANUAL'
+			elif(dependientes > 0):
+				return 'SOLICITUD APROBADA'
+
